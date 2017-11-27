72b4022bc29b70eb6275a91fb7d90c23db4d8dc5
Stefan Busemann <stefan.busemann@in2code.de>
2017-11-27 11:36:58 +0100

    [TASK] Add compiled settings

--- Classes/Controller/UserProfileController.php
+++ Classes/Controller/UserProfileController.php
@@ -48,6 +48,8 @@ class UserProfileController extends ActionController
         }
         // display a profile of a user
         $this->view->assign('userProfile',$userProfile);
+        
+        $this->view->assign('privacySettings',$userProfile->getCompiledPrivacySettings($this->settings['privacy']));
         // render an edit button, if the current user is logged in
     }
 
