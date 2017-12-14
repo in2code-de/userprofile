0c3b6e92489eac8f8d6d85f0234b7b3061592dbf
Roland Waldner <roland.waldner@in2code.de>
2017-12-14 17:12:23 +0100

    [CLEANUP] Add missing colons in arrays

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
         'depends' => [
             'typo3' => '8.7.0-8.99.99',
             'php' => '7.0.0-7.99.99',
-            'femanager' => '3.1.0-'
+            'femanager' => '3.1.0-',
         ],
         'conflicts' => [],
         'suggests' => [],
