1df467bf6dd7759012b25713ad17c5fb1891efad
Thomas Löffler <loeffler@spooner-web.de>
2019-05-21 08:37:16 +0200

    Set constraints to v9 in ext_emconf and composer.json

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.2.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.99.99',
+            'typo3' => '8.7.0-9.5.99',
             'php' => '7.0.0-7.99.99',
         ],
         'conflicts' => [],
