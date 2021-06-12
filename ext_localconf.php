b7e97eb00ce52e502fbcbfa5b652c706920a886f
Stefan Busemann <stefan.busemann@in2code.de>
2021-06-12 16:50:07 +0200

    [BUGFIX] Register and configer Plugins for TYPO3 10

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,24 +1,29 @@
 <?php
+
 defined('TYPO3_MODE') or die('Access denied.');
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'In2code.' . $_EXTKEY,
-    'Pi1',
-    [
-        'UserProfile' => 'show,edit,update,privacyUpdate,privacyEdit,changeProfileVisibility,list',
-    ],
-    [
-        'UserProfile' => 'show,edit,update,privacyUpdate,privacyEdit,changeProfileVisibility',
-    ]
-);
+call_user_func(
+    function () {
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'userprofile',
+            'Pi1',
+            [
+                \In2code\Userprofile\Controller\UserProfileController::class => 'show,edit,update,privacyUpdate,privacyEdit,changeProfileVisibility,list',
+            ],
+            [
+                \In2code\Userprofile\Controller\UserProfileController::class => 'show,edit,update,privacyUpdate,privacyEdit,changeProfileVisibility',
+            ]
+        );
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'In2code.' . $_EXTKEY,
-    'Pi2',
-    [
-        'UserProfile' => 'list,show,edit,update',
-    ],
-    [
-        'UserProfile' => 'show,edit,update',
-    ]
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'userprofile',
+            'Pi2',
+            [
+                \In2code\Userprofile\Controller\UserProfileController::class => 'list,show,edit,update',
+            ],
+            [
+                \In2code\Userprofile\Controller\UserProfileController::class => 'show,edit,update',
+            ]
+        );
+    }
 );
--- ext_tables.php
+++ ext_tables.php
@@ -4,5 +4,5 @@ defined('TYPO3_MODE') or die('Access denied.');
 /*
  * FE Plugin
  */
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin('In2code.' . $_EXTKEY, 'Pi1', 'Show profile for a Frontend User');
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin('In2code.' . $_EXTKEY, 'Pi2', 'List profile for Frontend Users');
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin('Userprofile', 'Pi1', 'Show profile for a Frontend User');
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin('Userprofile', 'Pi2', 'List profile for Frontend Users');
