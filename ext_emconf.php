12255bc89a34805ccde12f1b9385cdf315b41f1b
Stefan Busemann <stefan.busemann@in2code.de>
2021-06-12 15:22:09 +0200

    [TASK] Add DDEV dev env

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,9 +1,10 @@
 <?php
+
 declare(strict_types=1);
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'User profile',
-    'description' => 'Creates a profile page for TYPO3 frontend user based on Extbase and Fluid and on TYPO3 8.7',
+    'description' => 'Creates a profile page for TYPO3 frontend user based on Extbase and Fluid and allows frontend users to maintain it',
     'category' => 'plugin',
     'author' => 'Stefan Busemann',
     'author_email' => 'stefan@in2code.de',
@@ -11,18 +12,18 @@ $EM_CONF[$_EXTKEY] = [
     'shy' => '',
     'priority' => '',
     'module' => '',
-    'state' => 'alpha',
+    'state' => 'beta',
     'internal' => '',
     'uploadfolder' => '0',
     'createDirs' => '',
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
     'lockType' => '',
-    'version' => '0.2.1',
+    'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-8.99.99',
-            'php' => '7.0.0-7.99.99',
+            'typo3' => '10.4.0-10.4.99',
+            'php' => '7.2.0-7.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
